e762b2028e38dc05cc20da6e856bfba66c7f2da5
ctyode <irribarrenya@gmail.com>
2016-07-21 23:27:00 +0300

    Video save

--- app/src/main/java/org/flamie/flamethrower/ui/MainObjects.java
+++ app/src/main/java/org/flamie/flamethrower/ui/MainObjects.java
@@ -4,24 +4,32 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import org.flamie.flamethrower.callback.PictureCallback;
 import org.flamie.flamethrower.callback.PreviewCallback;
 import org.flamie.flamethrower.objects.BottomPanel;
-import org.flamie.flamethrower.objects.ConfirmationPanel;
 import org.flamie.flamethrower.objects.buttons.ButtonAccept;
 import org.flamie.flamethrower.objects.buttons.ButtonCapture;
 import org.flamie.flamethrower.objects.buttons.ButtonChange;
 import org.flamie.flamethrower.objects.buttons.ButtonDecline;
+import org.flamie.flamethrower.util.ImageSave;
 import org.flamie.flamethrower.util.PreviewUtils;
 
-public class MainObjects extends FrameLayout implements PreviewCallback.CameraListener {
+import java.io.File;
+import java.io.IOException;
+
+public class MainObjects extends RelativeLayout implements PreviewCallback.CameraListener {
 
     private static final String TAG = "MainObjects";
     public static boolean safeToTakePicture = false;
@@ -32,11 +40,14 @@ public class MainObjects extends FrameLayout implements PreviewCallback.CameraLi
     private PictureCallback mPicture;
     private Camera mCamera;
     private PreviewUtils previewUtils;
+    private MediaRecorder mediaRecorder;
+    private File videoFile;
 
-    private ConfirmationPanel confirmationPanel;
+    private BottomPanel confirmationPanel;
     private ImageView photoPreview;
     private ButtonAccept buttonAccept;
     private ButtonDecline buttonDecline;
+    private boolean isRecord = false;
 
     public MainObjects(Context context, Activity activity) {
         super(context);
@@ -50,8 +61,11 @@ public class MainObjects extends FrameLayout implements PreviewCallback.CameraLi
     }
 
     private void init() {
-        confirmationPanel = new ConfirmationPanel(getContext());
+        Button videoButton = new Button(getContext());
+
+        confirmationPanel = new BottomPanel(getContext());
         photoPreview = new ImageView(getContext());
+        photoPreview.setBackgroundColor(Color.rgb(0, 0, 0));
         buttonAccept = new ButtonAccept(getContext());
         buttonDecline = new ButtonDecline(getContext());
 
@@ -64,11 +78,25 @@ public class MainObjects extends FrameLayout implements PreviewCallback.CameraLi
         buttonAccept.setVisibility(INVISIBLE);
         buttonDecline.setVisibility(INVISIBLE);
 
-        LayoutParams photoPreviewParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        LayoutParams photoPreviewParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         LayoutParams captureButtonParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         LayoutParams buttonChangeParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         LayoutParams bottomPanelParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        LayoutParams previewParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        LayoutParams previewParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        photoPreviewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        photoPreview.setScaleType(ImageView.ScaleType.FIT_START);
+
+        videoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isRecord) {
+                    onClickStopRecord();
+                } else {
+                    onClickStartRecord();
+                }
+            }
+        });
 
         buttonCapture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -127,6 +155,8 @@ public class MainObjects extends FrameLayout implements PreviewCallback.CameraLi
         addView(confirmationPanel);
         addView(buttonAccept);
         addView(buttonDecline);
+
+        addView(videoButton);
     }
 
     public synchronized Camera getCameraInstance() {
@@ -163,6 +193,57 @@ public class MainObjects extends FrameLayout implements PreviewCallback.CameraLi
         retVal = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
 
         return retVal;
+    }
+
+    public void onClickStartRecord() {
+        if (prepareVideoRecorder()) {
+            mediaRecorder.start();
+            isRecord = true;
+        } else {
+            releaseMediaRecorder();
+        }
+    }
+
+    public void onClickStopRecord() {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            isRecord = false;
+            releaseMediaRecorder();
+        }
+    }
+
+    private boolean prepareVideoRecorder() {
+        mCamera = getCameraInstance();
+        mCamera.setDisplayOrientation(previewUtils.cameraRotation(mPreview.getCameraId()));
+        mediaRecorder = new MediaRecorder();
+        mCamera.unlock();
+        mediaRecorder.setCamera(mCamera);
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        mediaRecorder.setOutputFile(ImageSave.getOutputMediaFile(2).toString());
+        mediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
+        try {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            mCamera.lock();
+        }
     }
 
     @Override
